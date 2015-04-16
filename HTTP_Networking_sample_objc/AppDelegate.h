222a34dd92bc041db38c987369d920a2ad2b6a2c
Kohei Hayakawa <kohei.hayakawa.5@gmail.com>
2015-04-16 15:19:18 +0900

    Create project

---
+++ HTTP_Networking_sample_objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTP_Networking_sample_objc
+//
+//  Created by Kohei Hayakawa on 4/16/15.
+//  Copyright (c) 2015 Kohei Hayakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTTP_Networking_sample_objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HTTP_Networking_sample_objc
+//
+//  Created by Kohei Hayakawa on 4/16/15.
+//  Copyright (c) 2015 Kohei Hayakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
